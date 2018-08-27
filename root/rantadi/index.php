3b51fe7c0f5abfdf2b4eb0f2b5963df631754e69
Hasol Im <jerubball@gmail.com>
2018-08-27 15:20:50 -0400

    update 12

--- root/rantadi/index.php
+++ root/rantadi/index.php
@@ -45,57 +45,103 @@
         }
     </script>
 <?php
-        $cutoff = 6;
-        $collection = [
-            ["mission" => "hidra", "description" => "하이드라의 저주 : 미네랄 400, 가스 400",
-             "data" => [
-                ["class" => "U",
-                 "tower" => [
-                    ["name" => "U Hydralisk", "index" => 3, "title" => "히드라 3"],
-                ]],
+    $width="150";
+    $height="100";
+    $cutoff = 6;
+    $collection = [
+        ["mission" => "flying", "description" => "날아올라라 주작이여 : 보스 시민 2기", "appendix" => "<strong>(★★ 에픽 제외 모든 공중유닛 1기씩 ★★)</strong><br>"
+         "data" => [
+            ["class" => "U",
+             "tower" => [
+                ["name" => "U Valkyrie", "index" => 1, "title" => "발키리"],
+                ["name" => "U Guardian", "index" => 1, "title" => "가디언"],
             ]],
-            ["mission" => "crazy", "description" => "어느 미치광이의 폭주 : 미네랄 100, 가스 525",
-             "data" => [
-                ["class" => "E",
-                 "tower" => [
-                    ["name" => "E Nuclear", "index" => 2, "title" => "핵 2"],
-                ]],
-                ["class" => "M",
-                 "tower" => [
-                    ["name" => "M MGhost", "index" => 2, "title" => "남자 고스트 2"],
-                ]],
+            ["class" => "R",
+             "tower" => [
+                ["name" => "R Devourer", "index" => 1, "title" => "디바우러"],
+                ["name" => "R Arbiter", "index" => 1, "title" => "아비터"],
+                ["name" => "R Wraith", "index" => 1, "title" => ""],
             ]],
-            ["mission" => "hellgate", "description" => "더 키스 오브 헬 게이트 : 유니크 시민 2기",
-             "data" => [
-                ["class" => "E",
-                 "tower" => [
-                    ["name" => "E Hell Fire", "index" => 3, "title" => "지옥불 3"],
-                ]],
+            ["class" => "M",
+             "tower" => [
+                ["name" => "M Scout", "index" => 1, "title" => "스카웃"],
             ]],
-            ["mission" => "orangeannoying", "description" => "귤먹다가 짜증이 : 미네랄 300",
-             "data" => [
-                ["class" => "U",
-                 "tower" => [
-                    ["name" => "U Reaver", "index" => 3, "title" => "리버 3"],
-                ]],
+            ["class" => "N",
+             "tower" => [
+                ["name" => "N Mutalisk", "index" => 1, "title" => "뮤탈"],
+                ["name" => "N Scout", "index" => 1, "title" => "스카웃"],
+                ["name" => "N Wraith", "index" => 1, "title" => "레이스"],
             ]],
-            ["mission" => "bigbigreaver", "description" => "카리스마 대빵 큰 리버 : 미네랄 800, 가스 800",
-             "data" => [
-                ["class" => "U",
-                 "tower" => [
-                    ["name" => "U Reaver", "index" => 5, "title" => "리버 5"],
-                ]],
+        ]],
+        ["mission" => "bsss", "description" => "비선실세 : 가스 503",
+         "data" => [
+            ["class" => "U",
+             "tower" => [
+                ["name" => "U Guardian", "index" => 1, "title" => "가디언"],
+                ["name" => "U Dark Archo", "index" => 1, "title" => "다크 아칸"],
+                ["name" => "U High Templar", "index" => 1, "title" => "하이 템플러"],
             ]],
-        ];
-        /*
-            ["mission" => "", "description" => "", "appendix" => ""
-             "data" => [
-                ["class" => "",
-                 "tower" => [
-                    ["name" => "", "index" => 0, "title" => ""],
-                ]],
+            ["class" => "R",
+             "tower" => [
+                ["name" => "R Devourer", "index" => 1, "title" => "디바우러"],
+                ["name" => "R Sunken", "index" => 1, "title" => "촉수"],
             ]],
-        */
+            ["class" => "M",
+             "tower" => [
+                ["name" => "M Defiler", "index" => 1, "title" => "디파일러"],
+                ["name" => "M Dark Templar", "index" => 1, "title" => "다크 템플러"],
+                ["name" => "M Scout", "index" => 1, "title" => "스카웃"],
+            ]],
+        ]],
+        ["mission" => "hidra", "description" => "하이드라의 저주 : 미네랄 400, 가스 400",
+         "data" => [
+            ["class" => "U",
+             "tower" => [
+                ["name" => "U Hydralisk", "index" => 3, "title" => "히드라 3"],
+            ]],
+        ]],
+        ["mission" => "crazy", "description" => "어느 미치광이의 폭주 : 미네랄 100, 가스 525",
+         "data" => [
+            ["class" => "E",
+             "tower" => [
+                ["name" => "E Nuclear", "index" => 2, "title" => "핵 2"],
+            ]],
+            ["class" => "M",
+             "tower" => [
+                ["name" => "M MGhost", "index" => 2, "title" => "남자 고스트 2"],
+            ]],
+        ]],
+        ["mission" => "hellgate", "description" => "더 키스 오브 헬 게이트 : 유니크 시민 2기",
+         "data" => [
+            ["class" => "E",
+             "tower" => [
+                ["name" => "E Hell Fire", "index" => 3, "title" => "지옥불 3"],
+            ]],
+        ]],
+        ["mission" => "orangeannoying", "description" => "귤먹다가 짜증이 : 미네랄 300",
+         "data" => [
+            ["class" => "U",
+             "tower" => [
+                ["name" => "U Reaver", "index" => 3, "title" => "리버 3"],
+            ]],
+        ]],
+        ["mission" => "bigbigreaver", "description" => "카리스마 대빵 큰 리버 : 미네랄 800, 가스 800",
+         "data" => [
+            ["class" => "U",
+             "tower" => [
+                ["name" => "U Reaver", "index" => 5, "title" => "리버 5"],
+            ]],
+        ]],
+    ];
+    /*
+        ["mission" => "", "description" => "", "appendix" => ""
+         "data" => [
+            ["class" => "",
+             "tower" => [
+                ["name" => "", "index" => 0, "title" => ""],
+            ]],
+        ]],
+    */
 ?>
     <h1>랜타디 ReZero 미션 목록</h1>
     <br>
@@ -151,8 +197,6 @@
           ?></td><td align="<?php echo $align; ?>">
 <?php
                 }
-?>
-<?php
                 for ($l = 0; $l < $towerset["index"]; $l++) {
                     if ($l == $cutoff) {
 ?>
@@ -161,7 +205,7 @@
                     }
 ?>
                 <input type="checkbox" onclick="toggleClicked(this)"
-                    data-mission="<?php echo $missionset["mission"]; ?>" data-tower="<?php echo $towerset["name"]; ?>" data-index="<?php echo $l; ?>"/>
+                    data-mission="<?php echo $missionset["mission"]; ?>" data-tower="<?php echo $towerset["name"]; ?>" data-index=<?php echo $l; ?>/>
 <?php
                 }
                 $temp = $towerset["index"] - 2 - ($towerset["index"] > $cutoff ? 1 : 0);
@@ -180,8 +224,8 @@
                 $towerset = $classset["tower"][$k];
                 for ($l = 0; $l < $towerset["index"]; $l++) {
 ?>
-            <td><img width=150 height=100 onclick="toggleClicked(this)" src="./image/<?php echo $towerset["name"]; ?>.png" 
-                data-mission="<?php echo $missionset["mission"]; ?>" data-tower="<?php echo $towerset["name"]; ?>" data-index="<?php echo $l; ?>"></td>
+            <td><img src="./image/<?php echo $towerset["name"]; ?>.png" width="<?php echo $width; ?>" height="<?php echo $height; ?>" onclick="toggleClicked(this)" 
+                data-mission="<?php echo $missionset["mission"]; ?>" data-tower="<?php echo $towerset["name"]; ?>" data-index=<?php echo $l; ?>></td>
 <?php
                 }
             }
