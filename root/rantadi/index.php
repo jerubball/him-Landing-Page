646070b3ca1782787bfb71df716bca2c8b51b517
Hasol Im <jerubball@gmail.com>
2018-08-28 00:24:10 -0400

    update page

--- root/rantadi/index.php
+++ root/rantadi/index.php
@@ -78,8 +78,37 @@
 <?php
     $width="150";
     $height="100";
+    $offset = 4;
     $cutoff = 6;
     $collection = [
+        ["mission" => "", "description" => "", "preface" => "",
+         "data" => [
+            ["class" => "",
+             "tower" => [
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+            ]],
+        ]],
+        ["mission" => "", "description" => "", "preface" => "",
+         "data" => [
+            ["class" => "",
+             "tower" => [
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+                ["name" => "", "index" => 1, "title" => ""],
+            ]],
+        ]],
         ["mission" => "strangehobby", "description" => "이상한 사람의 취미생활 : 보스 시민 3기", "preface" => "(★ 리버를 제외한 모든 <b>U</b> 유닛 1기씩 ★)<br>",
          "data" => [
             ["class" => "U",
@@ -354,17 +383,17 @@
         <strong>2. 지긋지긋해 하늘이 Gray색이야 : 미네랄 300, 가스 100<br></strong>== <strong>M</strong> 종류별로 1기씩<br>
     </fieldset></form>
     <br>
-    <form><fieldset>
+    <form id=1 ><fieldset>
         <legend><strong>언럭키세븐 : 미네랄 300</strong></legend>
         (★ <strong>R</strong> 종류 상관없이 7기 ★)<br>
     </fieldset></form>
     <br>
-    <form><fieldset>
+    <form id=2 ><fieldset>
         <legend><strong>악운도 운이다 : 미네랄 300</strong></legend>
         (★ <strong>U</strong> 종류 상관없이 6기 ★)<br>
     </fieldset></form>
     <br>
-    <form><fieldset>
+    <form id=3 ><fieldset>
         <legend><strong>천년의 기다림 : 유니크 시민 1기</strong></legend>
         (★ 보스 시민 5기 보유 ★)<br>
     </fieldset></form>
@@ -374,7 +403,7 @@
     for ($i = 0; $i < $missions; $i++) {
         $missionset = $collection[$i];
 ?>
-    <form data-mission="<?php echo $missionset["mission"]; ?>"><fieldset>
+    <form id=<?php echo ($i + $offset); ?> data-mission="<?php echo $missionset["mission"]; ?>"><fieldset>
         <legend><input type="radio" disabled=true
             id="<?php echo $missionset["mission"]; ?>" data-mission="<?php echo $missionset["mission"]; ?>"/>
         <strong><?php echo $missionset["description"]; ?></strong></legend>
@@ -465,6 +494,7 @@
         <strong>2018.08.24</strong> : 체크 박스 스크립트 추가<br>
         <strong>2018.08.25</strong> : 타워 스크린샷 추가<br>
         <strong>2018.08.26</strong> : 체크 박스 개수 추가<br>
+        <strong>2018.08.28</strong> : 미션 정리<br>
     </fieldset></form>
     <br>
     <font color="blue"><strong>Made by aMythos (Mythical#31274)</strong></font>
