e073486e8ce8982ecd40e1018fe061d2068257d8
Hasol Im <jerubball@gmail.com>
2018-10-09 19:31:07 -0400

    Update install.sh

--- root/scripts/install.sh
+++ root/scripts/install.sh
@@ -2,12 +2,22 @@
 
 list="apache2 default-jdk default-mysql-client default-mysql-server docker emacs exfat-fuse exfat-utils fdisk fuseiso gcc gimp git htop hwinfo mysql-client mysql-server mysql-workbench nano net-tools nodejs openjdk-11-jdk openjdk-8-jdk openssh-client openssh-server openssl openvpn php php-all-dev python python3 rsync screen ssh telnet texlive texworks vim vsftpd wine-stable"
 
-sudo -s
+if [[ $# -gt 0 ]]
+then
+    mode=0
+else
+    mode=1
+fi
 
-apt update
-apt upgrade
+if [[ $mode -gt 0 ]]
+then
+    sudo install.sh all
+else
+    apt update
+    apt upgrade
 
-apt install -y $list
+    apt install -y $list
 
-apt update
-apt upgrade
+    apt update
+    apt upgrade
+fi
