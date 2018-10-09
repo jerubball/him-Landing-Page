f324e203acb06fe354527e3993a0240ced578d14
Hasol Im <jerubball@gmail.com>
2018-10-09 19:41:42 -0400

    Update install.sh

--- root/scripts/install.sh
+++ root/scripts/install.sh
@@ -1,17 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 list="apache2 default-jdk default-mysql-client default-mysql-server docker emacs exfat-fuse exfat-utils fdisk fuseiso gcc gimp git htop hwinfo mysql-client mysql-server mysql-workbench nano net-tools nodejs openjdk-11-jdk openjdk-8-jdk openssh-client openssh-server openssl openvpn php php-all-dev python python3 rsync screen ssh telnet texlive texworks vim vsftpd wine-stable"
 
-if [[ $# -gt 0 ]]
+if [[ $(id -u) -ne 0 ]]
 then
-    mode=0
-else
-    mode=1
-fi
-
-if [[ $mode -eq 0 ]]
-then
-    sudo install.sh all
+    sudo install.sh
 else
     apt update
     apt upgrade
