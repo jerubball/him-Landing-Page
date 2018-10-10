8385af175fe21a4fd91105c87783f5ad1f9c3923
Hasol Im <jerubball@gmail.com>
2018-10-09 22:48:47 -0400

    Update mysql.sql

--- root/scripts/mysql.sql
+++ root/scripts/mysql.sql
@@ -1,5 +1,5 @@
 -- use with mysql < mysql.sql
 
-create user 'ieee'@'%' identified BY 'ieee';
-grant all on *.* to 'ieee'@'%';
+create user 'ieee'@'%' identified by 'ieee';
+grant all on *.* to 'ieee'@'%' with grant option;
 flush privileges;
