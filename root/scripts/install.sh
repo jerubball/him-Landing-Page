237f18a9eb2826adf1d1e422e2cfb2f86f9baf5b
Hasol Im <jerubball@gmail.com>
2018-10-22 13:30:31 -0400

    Update install.sh

--- root/scripts/install.sh
+++ root/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-list="anacron apache2 apt boinc cron default-jdk default-mysql-client default-mysql-server dialog docker dpkg easy-rsa emacs exfat-fuse exfat-utils fdisk fuseiso gcc gimp git htop hwinfo mysql-client mysql-server mysql-workbench nano net-tools nodejs openjdk-11-jdk openjdk-8-jdk openssh-client openssh-server openssl openvpn php php-all-dev python python-pip python3 rsync screen ssh sudo telnet texlive texworks vim virtualbox virtualbox-qt vsftpd wget wine-stable wine32 wine64 winetricks yum"
+list="anacron apache2 apt boinc cron default-jdk default-mysql-client default-mysql-server dialog docker dpkg easy-rsa emacs exfat-fuse exfat-utils fdisk fuseiso gcc gimp git htop hwinfo mysql-client mysql-server mysql-workbench nano net-tools nodejs numlockx openjdk-11-jdk openjdk-8-jdk openssh-client openssh-server openssl openvpn php php-all-dev python python-pip python3 rsync screen ssh sudo telnet texlive texworks vim virtualbox virtualbox-qt vsftpd wget wine-stable wine32 wine64 winetricks yum"
 
 if [[ $(id -u) -ne 0 ]]
 then
