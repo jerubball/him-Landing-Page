2e1c58cf8319be33c435708b3165941c0fcff248
Hasol Im <jerubball@gmail.com>
2018-10-25 17:47:43 -0400

    update 1

---
+++ root/cluster/README.md
@@ -0,0 +1,5 @@
+
+
+
+
+https://github.com/BlackrockDigital/startbootstrap-resume
--- root/scripts/him-get.sh
+++ root/scripts/him-get.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+pass=false
+keep=false
+
 if [[ $(id -u) -ne 0 ]]
 then
     sudo ./$0 $@
 else
     while [[ $# > 0 ]]
     do
-        wget him-nyit.ddns.net/scripts/$1.sh -O $1.sh
-        chmod +x $1.sh
-        ./$1.sh
-        rm $1.sh
+        if [[ $1 == "--help"     ]]
+        then
+            echo "
+him-get: script executer from him-nyit.ddns.net
+usage: ./him-get.sh [OPTIONS] SCRIPTS
+"
+            exit
+        elif [[ $1 == "--pass" ]]
+        then
+        else
+            wget him-nyit.ddns.net/scripts/$1.sh -O $1.sh
+            chmod +x $1.sh
+            ./$1.sh
+            rm $1.sh
+        fi
         shift
     done
 fi
