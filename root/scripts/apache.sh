4fc088f26a277abff88b854e41cd34957055d874
Hasol Im <jerubball@gmail.com>
2018-10-29 22:04:45 -0400

    Update root/scripts/apache.sh

---
+++ root/scripts/apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+chmod -R 775 /etc/apache2
+chgrp -R adm /etc/apache2
+
+chmod -R 775 /var/www
+chgrp -R adm /var/www
+
+chmod -R 775 /usr/lib/apache2
+chgrp -R adm /usr/lib/apache2
+
+chmod -R 775 /var/lib/apache2
+chgrp -R adm /var/lib/apache2
+
+chmod -R 775 /usr/share/apache2
+chgrp -R adm /usr/share/apache2
+
