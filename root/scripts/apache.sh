4c2d755c0e13a52ee516d3ad9ffa54b2a1e88cff
Hasol Im <jerubball@gmail.com>
2018-11-08 17:17:45 -0500

    Update root/scripts/apache.sh

--- root/scripts/apache.sh
+++ root/scripts/apache.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-chmod -R 775 /etc/apache2
-chgrp -R adm /etc/apache2
 
-chmod -R 775 /var/www
-chgrp -R adm /var/www
+if [[ $(id -u) -ne 0 ]]
+then
+    sudo ./$0 $@
+else
+    chmod -R 775 /etc/apache2
+    chgrp -R adm /etc/apache2
 
-chmod -R 775 /usr/lib/apache2
-chgrp -R adm /usr/lib/apache2
+    chmod -R 775 /var/www
+    chgrp -R adm /var/www
 
-chmod -R 775 /var/lib/apache2
-chgrp -R adm /var/lib/apache2
+    chmod -R 775 /usr/lib/apache2
+    chgrp -R adm /usr/lib/apache2
 
-chmod -R 775 /usr/share/apache2
-chgrp -R adm /usr/share/apache2
+    chmod -R 775 /var/lib/apache2
+    chgrp -R adm /var/lib/apache2
 
+    chmod -R 775 /usr/share/apache2
+    chgrp -R adm /usr/share/apache2
+fi
