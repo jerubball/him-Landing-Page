9809a4f540576e0ed434db53232c999c0b9a3297
Hasol Im <jerubball@gmail.com>
2019-03-31 19:53:15 -0400

    separate header

--- root/index.php
+++ root/index.php
@@ -3,29 +3,11 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="google-site-verification" content="w1ig7tmYsNZOx3NG3Tl9Z7Qjhnr4SCSP5hj2PHQyp0Q" />
-  <!-- Bootstrap CDN -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-124973345-1"></script>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137014432-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-124973345-1');
-    gtag('config', 'UA-137014432-1');
-  </script>
-  <!-- Global tag -->
+<?php
+  require_once($_SERVER['DOCUMENT_ROOT'].'/header.html');
+?>
   <title>Landing Page</title>
-  <link rel="shortcut icon" href="/favicon-alt.ico">
-  <link rel="stylesheet" href="/wireframe.css">
-  <script src="/core.js"></script>
 </head>
 
 <body onload="setTitle('Landing Page')">
@@ -68,11 +50,6 @@
       <div class="row">
         <div class="col-md-12">
           <h1 class="display-3">Landing Page</h1>
-          
-<?php
-  require($_SERVER['DOCUMENT_ROOT'].'/test.php');
-  require($_SERVER['DOCUMENT_ROOT'].'/test.html');
-?>
         </div>
       </div>
     </div>
