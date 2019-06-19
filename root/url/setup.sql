f623733f95b029a57dc663c4c67e28d4f593add2
Hasol Im <jerubball@gmail.com>
2019-06-18 22:19:44 -0400

    Update setup.sql

--- root/url/setup.sql
+++ root/url/setup.sql
@@ -2,3 +2,14 @@ create database if not exists website;
 
 use website;
 
+create table if not exists url (
+  url  varchar(10)  primary key,
+  redirect varchar(100),
+  created datetime,
+  visited integer
+);
+
+-- alter table url modify url varchar(10) primary key;
+-- alter table url modify redirect varchar(100);
+-- alter table url modify created datetime;
+-- alter table url modify visited integer;
