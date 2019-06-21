052d03f05002d9d69c517aab2ad2e138011368b2
Hasol Im <jerubball@gmail.com>
2019-06-21 00:15:38 -0400

    Update setup.sql

--- root/url/setup.sql
+++ root/url/setup.sql
@@ -12,7 +12,9 @@ create table if not exists url (
   redirect  varchar(100)  not null,
   created  datetime,
   expires  datetime,
-  visited  integer  not null  default 0
+  visited  integer  not null  default 0,
+  enabled  boolean  not null  default true,
+  reserved  boolean  not null  default false
 );
 
 -- alter table url modify url varchar(10) primary key;
@@ -20,3 +22,5 @@ create table if not exists url (
 -- alter table url modify created datetime;
 -- alter table url modify expires datetime;
 -- alter table url modify visited integer not null default 0;
+-- alter table url modify enabled bool not null default true;
+-- alter table url modify reserved bool not null default false;
