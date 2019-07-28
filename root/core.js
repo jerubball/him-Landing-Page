97d3b6415b705bc28c3d2207ad748160ff2373f4
Hasol Im <jerubball@gmail.com>
2019-07-28 19:33:06 -0400

    Update core.js

--- root/core.js
+++ root/core.js
@@ -1,99 +1,139 @@
 
-const setTitle = function(text) {
-    var host = window.location.hostname;
-    var prefix = '';
-    if (host.includes('localhost')) {
-        prefix = 'localhost';
-    } else if (host.includes('hasol.co')) {
-        prefix = 'hasol.co';
-    } else if (host.includes('ddns.net')) {
-        prefix = 'him-NYIT';
-    } else {
-        prefix = 'him';
-    }
-    document.title = prefix + ': ' + text;
-}
+const Core = Object.freeze({
+    
+    
+    Window: Object.freeze({
+        
+        setTitle(text) {
+            var host = location.hostname;
+            var prefix = '';
+            if (host.includes('localhost')) {
+                prefix = 'localhost';
+            } else if (host.includes('hasol.co')) {
+                prefix = 'hasol.co';
+            } else if (host.includes('ddns.net')) {
+                prefix = 'him-NYIT';
+            } else {
+                prefix = 'him';
+            }
+            document.title = prefix + ': ' + text;
+        },
+        
+        setProtocol(proto, root) {
+            if (root) {
+                location.href = proto + '//' + location.hostname;
+            } else if (location.protocol != proto) {
+                location.protocol = proto;
+            }
+        },
+        
+        getSSLmode() {
+            return location.protocol == 'https:';
+        },
+        
+        setSSLindicator(item) {
+            if (Core.Window.getSSLmode()) {
+                item.classList.add('active');
+                item.setAttribute('aria-pressed', true);
+            }
+        },
+        
+        toggleProtocol() {
+            if (location.protocol == 'http:') {
+                location.protocol = 'https:';
+            } else if (location.protocol == 'https:') {
+                location.protocol = 'http:';
+            }
+        },
+        
+        setHostname(name, root) {
+            if (root) {
+                location.href = location.protocol + '//' + name;
+            } else if (location.hostname != name) {
+                location.hostname = name;
+            }
+        },
+        
+        setFullScreen(state) {
+            var current = document.fullscreen || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+            if (state && !current) {
+                if (document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen();
+                } else if (document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                }
+            } else if (!state && current) {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+            }
+        },
+        
+        toggleFullscreen() {
+            if (document.fullscreen || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+            } else {
+                if (document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen();
+                } else if (document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                }
+            }
+        },
+        
+        param: new URLSearchParams(location.search),
+        
+    }),
+    
+    
+    Math: Object.freeze({
+        
+        randomInt(min, max) {
+            if (max === undefined) {
+                return Math.floor(Math.random() * min);
+            } else {
+                return Math.floor(Math.random() * (max - min)) + min;
+            }
+        },
+        
+    }),
+    
+    
+});
 
-const setProtocol = function(proto, root) {
-    if (root) {
-        window.location.href = proto + '//' + window.location.hostname;
-    } else if (window.location.protocol != proto) {
-        window.location.protocol = proto;
+
+/*
+const Super = class {};
+const Super = function() {};
+class Super {
+    static #Sub = class {
+    };
+    static get Sub() {
+        return Super.#Sub;
     }
 }
-
-const getSSLmode = function() {
-    return window.location.protocol == 'https:';
-}
-
-const setSSLindicator = function(item) {
-    if (getSSLmode()) {
-        item.classList.add('active');
-        item.setAttribute('aria-pressed', true);
-    }
-}
-
-const toggleProtocol = function() {
-    if (window.location.protocol == 'http:') {
-        window.location.protocol = 'https:';
-    } else if (window.location.protocol == 'https:') {
-        window.location.protocol = 'http:';
-    }
-}
-
-const setHostname = function(name, root) {
-    if (root) {
-        window.location.href = window.location.protocol + '//' + name;
-    } else if (window.location.hostname != name) {
-        window.location.hostname = name;
-    }
-}
-
-const setFullScreen = function(state) {
-    var current = document.fullscreen || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
-    if (state && !current) {
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.mozRequestFullScreen) {
-            document.documentElement.mozRequestFullScreen();
-        } else if (document.documentElement.webkitRequestFullscreen) {
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) {
-            document.documentElement.msRequestFullscreen();
-        }
-    } else if (!state && current) {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-    }
-}
-
-const toggleFullscreen = function() {
-    if (document.fullscreen || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-    } else {
-        if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
-        } else if (document.documentElement.mozRequestFullScreen) {
-            document.documentElement.mozRequestFullScreen();
-        } else if (document.documentElement.webkitRequestFullscreen) {
-            document.documentElement.webkitRequestFullscreen();
-        } else if (document.documentElement.msRequestFullscreen) {
-            document.documentElement.msRequestFullscreen();
-        }
-    }
-}
+const Super = Object.freeze({Sub: class{},});
+Object.defineProperties(Super, {Sub: {value: class{}, writable: false, enumerable: false, configurable: false}, Subs: {value: function(){}}});
+*/
