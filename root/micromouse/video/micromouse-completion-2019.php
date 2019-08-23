6c5943de358c75f53499117f0e143e7d2b73cac4
Hasol Im <jerubball@gmail.com>
2019-08-23 16:12:12 -0400

    update video

---
+++ root/micromouse/video/micromouse-completion-2019.php
@@ -0,0 +1,11 @@
+<?php
+  $extension = '.mp4';
+  $filename = basename(__FILE__, '.php').$extension;
+  if (file_exists($filename)) {
+    header('Location: '.$filename);
+    exit();
+  } else {
+    header('Location: https://drive.google.com/uc?export=view&id=');
+    exit();
+  }
+?>
