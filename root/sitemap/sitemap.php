8a799a6f0b55e0aaf6293314541ba31601cffce2
Hasol Im <jerubball@gmail.com>
2019-10-15 23:11:31 -0400

    Update sitemap.php

--- root/sitemap/sitemap.php
+++ root/sitemap/sitemap.php
@@ -1,1 +1,21 @@
-
+<?php
+    $domain = '';
+    if (isset($_SERVER['REQUEST_SCHEME'])) {
+        $domain = $_SERVER['REQUEST_SCHEME'].'://';
+    } elseif (isset($_SERVER['HTTPS'])) {
+        $domain = 'https://';
+    } else {
+        $domain = 'http://';
+    }
+    if (isset($_SERVER['HTTP_HOST'])) {
+        $domain .= $_SERVER['HTTP_HOST'].'/';
+    } elseif (isset($_SERVER['SERVER_NAME'])) {
+        $domain .= $_SERVER['SERVER_NAME'].'/';
+    } else {
+        $domain .= 'hasol.co/';
+    }
+    
+    echo "\r\n";
+    echo $domain."\r\n";
+    
+?>
