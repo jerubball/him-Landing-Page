b30d351c7a7bc4fb424b3eff9eef2908225f85bd
Hasol Im <jerubball@gmail.com>
2019-11-25 20:02:57 -0500

    Update core.js

--- root/core.js
+++ root/core.js
@@ -142,7 +142,7 @@ const Core = Object.freeze({
         },
         
         toggleDarkMode() {
-            if (document.body.classList.contains('light') || (!document.body.classList.contains('dark') && Core.Window.getMediaColorSchemePreference() == 'dark')) {
+            if (document.body.classList.contains('light') || (!document.body.classList.contains('dark') && Core.Window.getMediaColorSchemePreference() != 'dark')) {
                 document.body.classList.remove('light');
                 document.body.classList.add('dark');
             } else {
