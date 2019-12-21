d609093f507f9a255f3c2f647715a73c341069ec
Hasol Im <jerubball@gmail.com>
2019-12-20 23:51:07 -0500

    Update compile.sh

--- root/scripts/compile.sh
+++ root/scripts/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version="
-him-compile version 1.8
+him-compile version 1.9
     quick edit and compiler from him-nyit.ddns.net
 "
 help="
@@ -84,7 +84,6 @@ do
         # default preset
         -d | --default )
             edit="nano -ASZcilm -T4 -$"
-            shift
         ;;
         # define editor
         -e | --editor )
