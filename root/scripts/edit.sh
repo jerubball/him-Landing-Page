78682a33b2424a1be7c0bb242db7fce886f49fde
Hasol Im <jerubball@gmail.com>
2020-01-16 16:19:19 -0500

    Create edit.sh

---
+++ root/scripts/edit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+filename=""
+if [[ "$filename" == "" ]]
+then
+    read -p "Enter file name: " filename
+fi
+
+editor=""
+if [[ "$editor" == "" ]]
+then
+    if [[ "$EDITOR" != "" ]]
+    then
+        editor="$EDITOR"
+    else
+        editor="nano"
+    fi
+fi
+
+if [[ ! -f $filename ]]
+then
+    touch $filename
+fi
+
+while true
+do
+    $editor $filename
+    while [[ $? -eq 147 ]]
+    do
+        fg
+    done
+done
