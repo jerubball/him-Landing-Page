6333d0c2d5ceabd7efe3ecf676f39be32ac8eb2a
Hasol Im <jerubball@gmail.com>
2020-04-05 19:37:22 -0400

    Create address.php

---
+++ root/chat/address.php
@@ -0,0 +1,12 @@
+<?php
+  
+  header("Content-Type: text/plain")."\n";
+  
+  echo getenv('HTTP_CLIENT_IP')."\n";
+  echo getenv('HTTP_X_FORWARDED_FOR')."\n";
+  echo getenv('HTTP_X_FORWARDED')."\n";
+  echo getenv('HTTP_FORWARDED_FOR')."\n";
+  echo getenv('HTTP_FORWARDED')."\n";
+  echo getenv('REMOTE_ADDR')."\n";
+  
+?>
