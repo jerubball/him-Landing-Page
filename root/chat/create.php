f3d72cfe151f800fcc2e64ff1c4f7db551b5e3c0
Hasol Im <jerubball@gmail.com>
2020-04-05 18:04:38 -0400

    Update create.php

--- root/chat/create.php
+++ root/chat/create.php
@@ -4,9 +4,16 @@
   
   if (!file_exists('.chat')) {
     fclose(fopen('.chat', 'w'));
+    echo 'metadata created.';
+  } else {
+    echo 'metadata exists.';
   }
+  echo "\r\n";
   if (!file_exists('chat')) {
     fclose(fopen('.chat', 'w'));
+    echo 'file created.';
+  } else {
+    echo 'file exists.';
   }
   
 ?>
