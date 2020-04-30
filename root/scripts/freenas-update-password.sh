8ef48fd9053523d677f050183cf37fa006e535d9
Hasol Im <jerubball@gmail.com>
2020-04-30 01:12:15 -0400

    Create freenas-update-password.sh

---
+++ root/scripts/freenas-update-password.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+userid=$(id -u)
+if [ $? -ne 0 ]
+then
+    echo "Unable to identify user"
+    exit 1
+fi
+
+if [ $userid -eq 0 ]
+then
+    echo "Cannot update password of root"
+    exit 1
+fi
+
+sudo -k
+echo "Authenticating user with sudo..."
+sudo echo "Success"
+
+if [ $? -ne 0 ]
+then
+    echo "Authentication failed"
+    exit 1
+fi
+    
+echo "Reading for new password..."
+passhash=$(openssl passwd -6)
+
+if [ $? -ne 0 ]
+then
+    echo "Password not registered"
+    exit 1
+fi
+
+sudo sqlite3 /data/freenas-v1.db "update account_bsdusers set bsdusr_unixhash = '$passhash' where bsdusr_uid = $userid"
+
+if [ $? -ne 0 ]
+then
+    echo "Database update failed"
+    exit 1
+fi
+
+exit 0
