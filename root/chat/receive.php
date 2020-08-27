4945d374bbff73204dfd6f88371ab088f45fe2e4
Hasol Im <jerubball@gmail.com>
2020-08-27 16:19:49 -0400

    Update receive.php

--- root/chat/receive.php
+++ root/chat/receive.php
@@ -9,37 +9,53 @@
   if (!$_SESSION['init']) {
     $response['code'] = 1;
     $response['status'] = 'Session not initialized.';
-  } elseif (!file_exists('.chat') || !file_exists('chat')) {
-    $response['code'] = 2;
-    $response['status'] = 'Chat room not found.';
   } else {
-    $_SESSION['lasttime'] = $_SESSION['timestamp'];
-    $_SESSION['timestamp'] = microtime(true);
-    $data = [];
-    $chat = fopen('chat', 'r');
-    flock($chat, LOCK_SH);
-    while (!feof($chat)) {
-        $line = fgets($chat);
-        // skip empty or commented line
-        if (strlen($line) > 0 && $line[0] !== '#') {
-            $items = explode("\t", $line);
-            // when time was after last check
-            if (sizeof($items) > 0) {
+    $id = $_SESSION['chatroom'];
+    $id_meta = '.'.$_SESSION['chatroom'];
+    if (!file_exists($id_meta)) {
+      $response['code'] = 2;
+      $response['status'] = 'Chat room not found.';
+    } else {
+      $metadata = json_decode(file_get_contents($id_meta));
+      if (!isset($metadata) || !is_array($metadata)) {
+        $response['code'] = 3;
+        $response['status'] = 'Unable to read metadata.';
+      } else {
+        $_SESSION['lasttime'] = $_SESSION['timestamp'];
+        $_SESSION['timestamp'] = microtime(true);
+        
+        $data = [];
+        if ($metadata['mode'] == 'text') {
+          $chat = fopen($id, 'r');
+          flock($chat, LOCK_SH);
+          while (!feof($chat)) {
+            $line = fgets($chat);
+            // skip empty or commented line
+            if (strlen($line) > 0 && $line[0] !== '#') {
+              $items = explode("\t", $line);
+              // when time was after last check
+              if (sizeof($items) > 0) {
                 $timestamp = floatval($items[0]);
                 if ($timestamp >= $_SESSION['lasttime'] && $timestamp < $_SESSION['timestamp']) {
-                    // use line.
-                    array_push($data, $line);
-                    // use json.
+                  // use line.
+                  array_push($data, $line);
+                  // use json.
                 }
+              }
             }
+          }
+          flock($chat, LOCK_UN);
+          fclose($chat);
+        } elseif ($metadata['mode'] == 'json') {
+          
+        } elseif ($metadata['mode'] == 'mysql') {
         }
+        
+        $response['code'] = 0;
+        $response['status'] = 'Success.';
+        $response['data'] = $data;
+      }
     }
-    flock($chat, LOCK_UN);
-    fclose($chat);
-    
-    $response['code'] = 0;
-    $response['status'] = 'Success.';
-    $response['data'] = $data;
   }
   
   echo json_encode($response);
