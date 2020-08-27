871524349fe08d34fd245b0670d3870c7e9d4e1e
Hasol Im <jerubball@gmail.com>
2020-08-27 16:07:32 -0400

    Update initialize.php

--- root/chat/initialize.php
+++ root/chat/initialize.php
@@ -6,12 +6,23 @@
   
   if (!$_SESSION['init']) {
     $_SESSION['init'] = true;
-    $_SESSION['username'] = base_convert(rand(), 10, 36);
+    
+    if (!isset($_SESSION['username'])) {
+      $_SESSION['username'] = base_convert(rand(), 10, 36);
+    }
   }
+  
   if (isset($_GET['all']) && $_GET['all'] == 'true') {
     $_SESSION['timestamp'] = 0;
   } else {
     $_SESSION['timestamp'] = microtime(true);
+  }
+  
+  $param = array_keys($_GET);
+  if (sizeof($param) > 0 && file_exists('.'.$param[0])) {
+    $_SESSION['chatroom'] = $param[0];
+  } else {
+    $_SESSION['chatroom'] = 'chat';
   }
   
 ?>
