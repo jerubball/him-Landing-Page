641ae3c95ee9c069e584be4dc30129adeb29022f
Hasol Im <jerubball@gmail.com>
2020-08-27 17:41:14 -0400

    Update send.php

--- root/chat/send.php
+++ root/chat/send.php
@@ -56,7 +56,7 @@
             $response['code'] = 6;
             $response['status'] = 'Unable to read chat.';
           } else {
-            array_push($data, ['time' => $timestamp, 'ip' => $items[1], 'name' => $items[2], 'text' => $items[3]]);
+            array_push($data, ['time' => microtime(true), 'ip' => $ip, 'name' => $_SESSION['username'], 'text' => $text);
             ftruncate($chat, 0);
             rewind($chat);
             fwrite($chat, json_encode($data));
