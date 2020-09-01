708ae3870748b677703624276fabaa0fa718f7df
Hasol Im <jerubball@gmail.com>
2020-09-01 17:58:13 -0400

    Update confirm.php

--- root/auth/confirm.php
+++ root/auth/confirm.php
@@ -1,5 +1,10 @@
 <?php
+  header('Content-Type: application/json');
+  
   require_once($_SERVER['DOCUMENT_ROOT'].'/core.php');
+  
+  $db_server = 'localhost';
+  $db_username = 'website.local';
   
   if (isset($_GET['email']) && strlen($_GET['email']) > 0 && preg_match('/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+\.[a-zA-Z]+$/', $_GET['email'])) {
     mail($_GET['email'], 'hasol.co authentication string', random_string());
