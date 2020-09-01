dc5dac82522343234cdd36644cd35bc5e589a3c2
Hasol Im <jerubball@gmail.com>
2020-09-01 18:30:20 -0400

    Update setup.sql

--- root/auth/setup.sql
+++ root/auth/setup.sql
@@ -10,8 +10,9 @@ use Website;
 create table if not exists authentication (
     email  varchar(255)  primary key,
     username  varchar(50),
+    ip  varchar(45),
     token  varchar(50),
     expires  datetime,
     suspended  datetime,
-    ip  varchar(45)
+    attempts  int
 );
