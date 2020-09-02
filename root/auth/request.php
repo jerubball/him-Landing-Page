10f073962544d8adbc339f4d7d4aac30b9f0bdad
Hasol Im <jerubball@gmail.com>
2020-09-01 21:05:09 -0400

    Update request.php

--- root/auth/request.php
+++ root/auth/request.php
@@ -12,9 +12,53 @@
     $response['code'] = 1;
     $response['status'] = 'No email provided.';
   } elseif (strlen($_GET['email']) < 255 && preg_match('/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+\.[a-zA-Z]+$/', $_GET['email'])) {
-    $token = random_string();
-    $subject = 'hasol.co authentication string';
-    $message = <<<EOS
+    $db_connection = new mysqli($db_server, $db_username);
+    if ($db_connection->connect_errno) {
+      $response['code'] = 2;
+      $response['status'] = 'Database connection failed.';
+    } elseif ($db_connection->begin_transaction(MYSQLI_TRANS_START_READ_WRITE)) {
+      
+      $email = $db_connection->real_escape_string($_GET['email']);
+      
+      // create entry if not exists
+      $db_query = 'select email, unix_timestamp(suspended) as suspended, attempts from Website.authentication where email = "'.$email.'";';
+      $db_answer1 = $db_connection->query($db_query);
+      if ($db_answer1) {
+        // success.
+        if ($db_answer1->num_rows > 0) {
+          // has info
+          $ans = $db_answer1->fetch_assoc();
+          if ($ans['suspended'] && $ans['suspended'] > time()) {
+            // email suspended.
+            $response['code'] = 3;
+            $response['status'] = 'Account is suspended for abuse.'
+          }
+        } else {
+          // no info
+          $db_query = 'insert into Website.authentication (email) values ("'.$email.'");';
+          $db_answer2 = $db_connection->query($db_query);
+          if (!$db_answer2) {
+            $response['code'] = 4;
+            $response['status'] = 'Database query failed.';
+          }
+          if ($db_answer2->free_result) {
+            $db_answer2->free_result();
+          }
+        }
+      } else {
+        // failed.
+        $response['code'] = 5;
+        $response['status'] = 'Database query failed.';
+      }
+      if ($db_answer1->free_result) {
+        $db_answer1->free_result();
+      }
+      
+      // proceed if no error
+      if (!isset($response['code']) {
+        $token = random_string();
+        $subject = 'hasol.co authentication string';
+        $message = <<<EOS
 <!DOCTYPE html>
 <html>
   <head>
@@ -39,14 +83,57 @@
   </body>
 </html>
 EOS;
-    $header = ['From' => '"hasol.co" <him.nyit@gmail.com>', 'Reply-To' => '"hasol.co" <postmaster@hasol.co>', 'MIME-Version' => '1.0', 'Content-Type' => 'text/html', 'X-Mailer' => 'PHP/'.phpversion()];
-    if (mail($_GET['email'], $subject, $message, $header)) {
+        $header = ['From' => '"hasol.co" <him.nyit@gmail.com>', 'Reply-To' => '"hasol.co" <postmaster@hasol.co>', 'MIME-Version' => '1.0', 'Content-Type' => 'text/html', 'X-Mailer' => 'PHP/'.phpversion()];
+        if (mail($_GET['email'], $subject, $message, $header)) {
+          $db_query = 'update Website.authentication set token = "'.$token.'", expires = adddate(now(), interval 1 week) where email = "'.$email.'";';
+          if (!$db_answer3) {
+            $response['code'] = 6;
+            $response['status'] = 'Database query failed.';
+          }
+          if ($db_answer3->free_result) {
+            $db_answer3->free_result();
+          }
+        } else {
+          $response['code'] = 7;
+          $response['status'] = 'Unable to send email.';
+        }
+        
+        // increment attempt and suspend if needed
+        $db_query = 'update Website.authentication set attempts = attempts + 1 where email = "'.$email.'"; update Website.authentication set suspended = adddate(now(), interval 1 week), attempts = 0 where attempts > 5;';
+        $db_answer4 = $db_connection->query($db_query);
+        if (!$db_answer4) {
+          $response['code'] = 8;
+          $response['status'] = 'Database query failed.';
+        }
+        if ($db_answer4->free_result) {
+          $db_answer4->free_result();
+        }
+      }
       
-      $response['code'] = 0;
-      $response['status'] = 'Success.';
+      // commit if no error
+      if (!isset($response['code']) {
+        if (!$db_connection->commit()) {
+          $response['code'] = 9;
+          $response['status'] = 'Unable to commit.';
+        }
+      }
+      
+      // rollback if error
+      if (!isset($response['code']) {
+        if (!$db_connection->rollback()) {
+          $response['code'] = 10;
+          $response['status'] = 'Unable to rollback.';
+        }
+      } else {
+        $response['code'] = 0;
+        $response['status'] = 'Success.';
+      }
     } else {
-      $response['code'] = 2;
-      $response['status'] = 'Unable to send email.';
+      $response['code'] = 3;
+      $response['status'] = 'Unable to start transaction.';
+    }
+    if ($db_connection->close) {
+      $db_connection->close();
     }
   } else {
     $response['code'] = 3;
