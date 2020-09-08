d1520154b51a9a17c37b623d5d84a15604ea9064
Hasol Im <jerubball@gmail.com>
2020-09-07 23:40:15 -0400

    update editor

--- root/editor/script.js
+++ root/editor/script.js
@@ -6,8 +6,33 @@ const Script = {
         return this.init(Core.None.__proto__);
     },
     
-    updateInfo(elem) {
+    setListeners() {
+        var elem = document.getElementById('text');
         var info = document.getElementById('info');
+        var listener = (e) => {
+            this.updateInfo(elem, info);
+        };
+        elem.onchange = listener;
+        elem.oninput = listener;
+        elem.oncopy = listener;
+        elem.oncut = listener;
+        elem.onpaste = listener;
+        elem.onkeydown = listener;
+        elem.onkeypress = listener;
+        elem.onkeyup = listener;
+        elem.onmousedown = listener;
+        elem.onclick = listener;
+        elem.onmouseup = listener;
+        elem.onmousemove = listener;
+    }
+    
+    updateInfo(elem, info) {
+        if (elem == undefined) {
+            elem = document.getElementById('text');
+        }
+        if (info == undefined) {
+            info = document.getElementById('info');
+        }
         if (elem.selectionStart == elem.selectionEnd) {
             info.innerHTML = elem.selectionStart + '/' + elem.textLength;
         } else {
